da7aa1354fc65f3f4a48952ece466df9c63083c1
Jorgosz Diamantisz <Jorgosz.Diamantisz1@hu.ibm.com>
2022-08-28 20:13:17 +0200

    use https

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const httpsOptions = {
+    key: fs.readFileSync('./src/secrets/localhost-private-key.pem'),
+    cert: fs.readFileSync('./src/secrets/localhost-public-key.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
